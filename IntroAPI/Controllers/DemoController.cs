98966e0077aa897daf30f6cecf18b33bcb65c95d
Faisal Ahmed Fahim <146206158+fahim-ahmed10@users.noreply.github.com>
2023-12-07 15:18:06 +0600

    first commit

---
+++ IntroAPI/Controllers/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroAPI.Controllers
+{
+    public class DemoController : ApiController
+    {
+        [HttpGet]
+        [Route("api/names")]
+        public HttpResponseMessage GetNames()
+        {
+            var names = new String[] { "Fahim", "Ahmed" };
+            return Request.CreateResponse(HttpStatusCode.OK, names);    
+        }
+        [HttpPost]
+        [Route("api/test/post")]
+        public HttpResponseMessage ReceivedPost()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Post received");
+        }
+    }
+}
